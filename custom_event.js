ba6047174e831cc87c798b5aa41b00ef692ba59c
Brady-Agranoff <brady.agranoff@gmail.com>
2023-12-08 08:48:00 -0800

    timeout validation

--- custom_event.js
+++ custom_event.js
@@ -56,6 +56,10 @@ if (mw_telemetry_settings.custom_event_configurations && mw_telemetry_settings.c
 			}
 
 			if (trigger.timeout) {
+				if (typeof trigger.timeout !== "number") {
+					throw new MasterworksTelemetryError("Invalid custom_event_configurations.triggers.timeout: " + trigger.timeout);
+				}
+
 				setTimeout(() => {
 					document.querySelectorAll(trigger.selector).forEach((element) => {
 						element.addEventListener(trigger.trigger_event, handleEvent);
