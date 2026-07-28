f0607a513870b75c972bcef837c44d229bf821d8
Masterworks <jcampbell@masterworks.agency>
2026-07-28 11:17:35 -0700

    Add datalayer_args_interval trigger type

--- telemetry.js
+++ telemetry.js
@@ -116,6 +116,14 @@ const mw_trigger_types = {
 		validateTriggerFields(trigger, ["event_name"]);
 		mw_trigger_detect_dataLayer_event_interval(trigger.event_name, callback);
 	},
+	// Like dataLayer_event_interval, but matches dataLayer entries pushed as gtag.js
+	// `arguments` objects (numeric-keyed: {"0":"event","1":"<event_name>","2":{...}}),
+	// e.g. sites with `function gtag(){dataLayer.push(arguments);}` calling
+	// gtag('event', 'purchase', {...}) directly, rather than a plain {event: "..."} literal.
+	datalayer_args_interval: (trigger, callback) => {
+		validateTriggerFields(trigger, ["event_name"]);
+		mw_trigger_detect_datalayer_args_interval(trigger.event_name, callback);
+	},
 	parameter_equals: (trigger, callback) => {
 		validateTriggerFields(trigger, ["parameter_key", "parameter_value"]);
 		mw_trigger_parameter_equals(trigger.parameter_key, trigger.parameter_value, callback);
@@ -277,6 +285,27 @@ function mw_trigger_detect_dataLayer_event_interval(event_name, callback) {
 			dataLayer[i][`mw_processed_${intervalDataLayerProcessID}`] = true;
 
 			if (dataLayer[i].event === event_name) {
+				callback();
+			}
+		}
+	}, 250);
+}
+
+function mw_trigger_detect_datalayer_args_interval(event_name, callback) {
+	// Same polling/mark-processed pattern as mw_trigger_detect_dataLayer_event_interval,
+	// but reads a gtag.js-style `arguments` object instead of a plain {event: "..."} literal:
+	// dataLayer.push(arguments) serializes as {"0": "event", "1": "<name>", "2": {...params}}.
+	const intervalDataLayerProcessID = Math.random().toString(36).substring(2, 15);
+	setInterval(function () {
+
+		for (let i = 0; i < dataLayer.length; i++) {
+			if (dataLayer[i][`mw_processed_${intervalDataLayerProcessID}`]) {
+				continue;
+			}
+
+			dataLayer[i][`mw_processed_${intervalDataLayerProcessID}`] = true;
+
+			if (dataLayer[i]["0"] === "event" && dataLayer[i]["1"] === event_name) {
 				callback();
 			}
 		}
